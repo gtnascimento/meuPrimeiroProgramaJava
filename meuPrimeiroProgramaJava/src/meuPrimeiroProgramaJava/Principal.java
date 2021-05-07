e7d13c92d432f34e79ec40745458d15458ae8438
biel1 <biel1@DESKTOP-TEQFIQN>
2021-05-07 13:23:46 -0300

    Alterações nos Comentários

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -17,15 +17,17 @@ import java.util.Scanner;
  */
 public class Principal { // Classe que contém o método principal.
 	public static void main(String[] args) { // Este é o método principal.
+		
+		// Saudação e pergunta o nome.
 		System.out.println("Olá amigo!\nQual é o seu nome?");
 		
-		// Ler uma string do console usando a biblioteca Scanner
+		// Ler o nome.
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		// Exibir um string usando printf.
+		// Saudação específica
 		System.out.printf("Olá %s!", nome);
-		in.close();
+		
 	}
 
 }

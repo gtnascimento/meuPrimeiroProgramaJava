39180c81ba06ac0d67929bf9e132d8c82f8eea8c
biel1 <biel1@DESKTOP-TEQFIQN>
2021-05-08 18:37:59 -0300

    Leitura da idade

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -2,9 +2,9 @@
  * Meu primeiro programa Java
  * 
  * Este programa mostra ao aluno:
- * 1) Que um programa Java √© constru√≠do dentro de um projeto Java.
- * 2) Todo programa Java deve ter uma classe com um m√©todo main.
- * 3) Como exibir informa√ß√µes no terminal (console) do Java.
+ * 1) Que um programa Java È construÌdo dentro de um projeto Java.
+ * 2) Todo programa Java deve ter uma classe com um mÈtodo main.
+ * 3) Como exibir informaÁıees no terminal (console) do Java.
  * 4) Como ler uma string do terminal (console) do Java.
  * 
  */
@@ -15,18 +15,26 @@ import java.util.Scanner;
 /**
  * @author biel1 data: 03/02/2021
  */
-public class Principal { // Classe que cont√©m o m√©todo principal.
-	public static void main(String[] args) { // Este √© o m√©todo principal.
+public class Principal { // Classe que contÈm o mÈtoodo principal.
+	public static void main(String[] args) { // Este È o mÈtodo principal.
 		
-		// Sauda√ß√£o e pergunta o nome.
-		System.out.println("Ol√° amigo!\nQual √© o seu nome?");
+		// SaudaÁ„o e pergunta o nome.
+		System.out.println("Ol·, amigo!\nQual È o seu nome?");
 		
 		// Ler o nome.
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		// Sauda√ß√£o espec√≠fica para o nome lido
-		System.out.printf("Ol√° %s!", nome);
+		// SaudaÁ„o para o nome lido
+		System.out.printf("Ol·! %s!\n", nome);
+		
+		//Perguntar a Idade
+		System.out.printf("%s, qual È a sua idade?", nome);
+		
+		//Ler idade.
+		Integer idade = in.nextInt();
+		
+		System.out.printf("%d! Puxa! Como vocÍ È jovem!", idade);
 		
 	}
 
